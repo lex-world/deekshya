de929f37d27099eba51bfa44095af6617a3ddb45
Laxman Rai <lex.world@protonmail.com>
2021-11-29 20:24:44 +0545

    Working on new UI & Added font loader, font

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
 import React from "react";
+import { useFonts } from "expo-font";
+import { ActivityIndicator } from "react-native";
 
 /** @context api */
-import UserDataLayer from "./Context/User/User";
-import ActiveTabDataLayer from "./Context/ActiveTabs/ActiveTab";
 
 /** @pages */
 import Layout from "./Layout";
 
 export default function App() {
-  return (
-    <UserDataLayer>
-      <ActiveTabDataLayer>
-        <Layout />
-      </ActiveTabDataLayer>
-    </UserDataLayer>
-  );
+  /**
+   * @dev Loading Fonts using expo-fonts
+   */
+  const [fontLoaded] = useFonts({
+    "Biryani-Black": require("./assets/fonts/Biryani-Black.ttf"),
+    "Biryani-Bold": require("./assets/fonts/Biryani-Bold.ttf"),
+    "Biryani-ExtraBold": require("./assets/fonts/Biryani-ExtraBold.ttf"),
+    "Biryani-ExtraLight": require("./assets/fonts/Biryani-ExtraLight.ttf"),
+    "Biryani-Light": require("./assets/fonts/Biryani-Light.ttf"),
+    "Biryani-Regular": require("./assets/fonts/Biryani-Regular.ttf"),
+    "Biryani-SemiBold": require("./assets/fonts/Biryani-SemiBold.ttf"),
+    "Lato-Black": require("./assets/fonts/Lato-Black.ttf"),
+    "Lato-BlackItalic": require("./assets/fonts/Lato-BlackItalic.ttf"),
+    "Lato-Bold": require("./assets/fonts/Lato-Bold.ttf"),
+    "Lato-BoldItalic": require("./assets/fonts/Lato-BoldItalic.ttf"),
+    "Lato-Italic": require("./assets/fonts/Lato-Italic.ttf"),
+    "Lato-Light": require("./assets/fonts/Lato-Light.ttf"),
+    "Lato-LightItalic": require("./assets/fonts/Lato-LightItalic.ttf"),
+    "Lato-Regular": require("./assets/fonts/Lato-Regular.ttf"),
+    "Lato-Thin": require("./assets/fonts/Lato-Thin.ttf"),
+    "Lato-ThinItalic": require("./assets/fonts/Lato-ThinItalic.ttf"),
+  });
+
+  if (!fontLoaded) return <ActivityIndicator />;
+  else return <Layout />;
 }
--- Layout.js
+++ Layout.js
@@ -1,18 +1,15 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 
-// @packages
+/**
+ * @packages
+ */
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-// @pages
-import LanguageSelection from "./Screens/UnAuthenticatedUserStack/LanguageSelection";
-import MobileNumberRegistration from "./Screens/UnAuthenticatedUserStack/MobileNumberRegistration";
-import PhoneVerification from "./Screens/UnAuthenticatedUserStack/PhoneVerification";
-import Greeting from "./Screens/UnAuthenticatedUserStack/Greeting";
-import UserKYC from "./Screens/UnAuthenticatedUserStack/UserKYC";
-import Home from "./Screens/AuthenticaterdUserStack/Home";
-import FeedFocused from "./Screens/AuthenticaterdUserStack/FeedFocused";
+/**
+ * @pages
+ */
 
 /**
  * * @dev Unauthenticated User Stack
@@ -20,52 +17,21 @@ import FeedFocused from "./Screens/AuthenticaterdUserStack/FeedFocused";
 const UserFlowStack = createNativeStackNavigator();
 
 /** @cont3xt api */
-import { useUserDataLayerValue } from "./Context/User/User";
+// import { useUserDataLayerValue } from "./Context/User/User";
 
 export default function Layout() {
-  const [{ authenticated }] = useUserDataLayerValue();
+  // const [{ authenticated }] = useUserDataLayerValue();
+
   return (
     <NavigationContainer>
       <StatusBar barStyle="dark-content" />
-      {/* section => Unauthenticated User stack */}
-      <UserFlowStack.Navigator
+      {/* <UserFlowStack.Navigator
         screenOptions={{
           headerShown: false,
         }}
-      >
-        {/* @dev if user is not authenticated */}
-        {!authenticated && (
-          <React.Fragment>
-            {/* @Screen => Language Selection */}
-            {/* <UserFlowStack.Screen
-              name="Language Selection"
-              component={LanguageSelection}
-            /> */}
-
-            {/* @Screen => Phone Number Registration */}
-            {/* <UserFlowStack.Screen
-              name="Phone Number Registration"
-              component={MobileNumberRegistration}
-            /> */}
-
-            {/* @Screen => Phone Number Verification */}
-            {/* <UserFlowStack.Screen
-              name="Phone Number Verification"
-              component={PhoneVerification}
-            /> */}
-
-            {/* @Screen => Greeting */}
-            {/* <UserFlowStack.Screen name="Greeting" component={Greeting} /> */}
-
-            {/* @Screen => User KYC */}
-            {/* <UserFlowStack.Screen name="User KYC" component={UserKYC} /> */}
-          </React.Fragment>
-        )}
-
-        {/* @dev if user is authenticated */}
+      > */}
         {/* <UserFlowStack.Screen name="Home" component={Home} /> */}
-        <UserFlowStack.Screen name="Feed Focused" component={FeedFocused} />
-      </UserFlowStack.Navigator>
+      {/* </UserFlowStack.Navigator> */}
     </NavigationContainer>
   );
 }
