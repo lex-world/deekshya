76e2812f8273c9c0759f95490b5ec212f55dbaaf
Laxman Rai <lex.world@protonmail.com>
2022-01-05 09:58:16 +0545

    Working on Home

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -14,7 +14,8 @@ import {
   View,
   TextInput,
 } from "react-native";
-import { Feather } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
 
 /**
  * @components custom main components
@@ -32,10 +33,21 @@ export default function HomeScreen() {
       {/** @section topbar */}
       <View style={styles.topBar}>
         <View style={styles.topBarLeft}>
-          <Text>Deekshya</Text>
+          <Text style={styles.logo}>Deekshya</Text>
         </View>
-        <View style={styles.topBarRight}></View>
+        <View style={styles.topBarRight}>
+          <TouchableOpacity style={styles.heart}>
+            <FontAwesome name="heart-o" size={24} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <AntDesign name="message1" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
       </View>
+
+      <Image style={styles.bannerImage} source={{
+        uri: "https://images.unsplash.com/photo-1605289982774-9a6fef564df8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+      }}/>
 
       {/** @section Reminders */}
       <View style={styles.reminders}>
@@ -59,11 +71,41 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   /** @section topbar */
-  topBar: {},
+  topBar: {
+    marginTop: 10,
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginHorizontal: Dimensions.get("window").width * 0.05,
+    borderBottomColor: "#dedede",
+    borderBottomWidth: 1,
+    paddingBottom: 15
+  },
+  logo: {
+    fontSize: 20,
+    fontFamily: "Biryani-Bold",
+  },
+  heart: {
+    marginRight: 15
+  },
+  topBarRight: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  bannerImage: {
+    width: Dimensions.get("window").width * 0.9,
+    alignSelf: "center",
+    height: Dimensions.get("window").height * 0.15,
+    resizeMode: "cover",
+    borderRadius: 10,
+    marginVertical: 20
+  },
 
   /** @section reminders */
   reminders: {
-    marginTop: 80,
     alignSelf: "center",
   },
   reminderTitle: {
